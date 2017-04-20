fd88e2be537a34f927cadcb68027e3201b38bd83
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-04-20 16:27:43 +0200

    Print: Wrap ZPL with ${ZPL}$ and configure printer with passthrou mode

--- src/Model/Behavior/PrintableBehavior.php
+++ src/Model/Behavior/PrintableBehavior.php
@@ -6,6 +6,6 @@ use Cake\ORM\Behavior;
 class PrintableBehavior extends Behavior
 {
     public function getZPL(string $code, string $description){
-        return "^XA^BY3,2,100^FO240,30^BC^FD".$code."^FS^CFA,30^FO255,190^FD".$description."^FS^XZ";
+        return '${^XA^BY3,2,100^FO240,30^BC^FD'.$code.'^FS^CFA,30^FO255,190^FD'.$description.'^FS^XZ}$';
     }
 }
