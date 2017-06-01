2c37bf83c0de846ee3c85c26833a78a56ade5e6d
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-06-01 22:45:14 +0200

    Refactor printable behavior.

--- src/Model/Behavior/PrintableBehavior.php
+++ src/Model/Behavior/PrintableBehavior.php
@@ -1,11 +1,22 @@
 <?php
+
 namespace App\Model\Behavior;
 
 use Cake\ORM\Behavior;
+use Cake\Core\Exception\Exception;
 
-class PrintableBehavior extends Behavior
-{
-    public function getZPL(string $code, string $description){
-        return '${^XA^BY3,2,100^FO240,30^BC^FD'.$code.'^FS^CFA,30^FO255,190^FD'.$description.'^FS^XZ}$';
-    }
+class PrintableBehavior extends Behavior {
+	public function getZPL( string $description, string $code = null ) {
+		if ( $code && $description ) {
+			return $this->_wrap( '^XA^BY3,2,100^FO240,30^BC^FD' . $code . '^FS^CFA,30^FO255,190^FD' . $description . '^FS^XZ' );
+		}
+		if ( $description ) {
+			return $this->_wrap( '^XA^CFA,30^FO255,115^FD' . $description . '^FS^XZ' );
+		}
+		throw new Exception( __( 'No printable content given.' ) );
+	}
+	
+	private function _wrap( string $zpl ) {
+		return '${' . $zpl . '}$';
+	}
 }
