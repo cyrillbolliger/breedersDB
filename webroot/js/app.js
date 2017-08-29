50954a4ced1e78657facaddf9e11ade05417b7d8
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-08-29 17:14:59 +0200

    JS code cosmetics

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1116,7 +1116,8 @@ $(document).ready(function () {
 })(jQuery);
 
 /**
- * Brings something like PHP's sprintf to js. Use it like "{0} string {1}".format("Handy", "replacement");
+ * Brings something like PHP's sprintf to js.
+ * Use it like "{0} string {1}".format("Handy", "replacement");
  *
  * @author: fearphage
  * @link: http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format/4673436#4673436
