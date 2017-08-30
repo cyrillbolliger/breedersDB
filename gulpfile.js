7901bc4f50c465565be74ea34a93cabea4dc779a
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-08-30 16:06:11 +0200

    Gulp: Add dist files

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,31 @@ gulp.task('imgsDev', function() {
         .pipe(gulp.dest(dev+'/img'));
 });
 
+gulp.task('imgsDist', function() {
+    gulp.src(src+'/img/**/*')
+        .pipe(gulp.dest(dist+'/img'));
+});
+
 // fonts
 gulp.task('fontsDev', function() {
     gulp.src(src+'/fonts/**/*')
         .pipe(gulp.dest(dev+'/fonts'));
 });
 
+gulp.task('fontsDist', function() {
+    gulp.src(src+'/fonts/**/*')
+        .pipe(gulp.dest(dist+'/fonts'));
+});
+
 // css
 gulp.task('cssDev', function() {
     gulp.src(src+'/css/**/*')
         .pipe(gulp.dest(dev+'/css'));
+});
+
+gulp.task('cssDist', function() {
+    gulp.src(src+'/css/**/*')
+        .pipe(gulp.dest(dist+'/css'));
 });
 
 // js
@@ -33,7 +48,14 @@ gulp.task('jsDev', function() {
         .pipe(gulp.dest(dev+'/js'));
 });
 
+gulp.task('jsDist', function() {
+    gulp.src(src+'/js/app.js')
+        .pipe(browserify())
+        .pipe(gulp.dest(dist+'/js'));
+});
+
 gulp.task('dev', ['imgsDev','fontsDev','cssDev','jsDev']);
+gulp.task('dist', ['imgsDist','fontsDist','cssDist','jsDist']);
 
 gulp.task('watch', function() {
     gulp.watch(src+'/img/**/*',['imgsDev']);
