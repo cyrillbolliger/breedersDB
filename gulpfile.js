b7ca0874737b7a214f497b5551ac824bdb281206
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-09-10 15:43:33 +0200

    Gulp cosmetics

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,68 @@
-const gulp = require('gulp');
-const browserify = require('gulp-browserify');
-const uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
 
 
-const src = 'webroot/src';
-const dev = 'webroot/dev';
-const dist = 'webroot/dist';
-
+var src = 'webroot/src';
+var dev = 'webroot/dev';
+var dist = 'webroot/dist';
 
 
 // imgs
-gulp.task('imgsDev', function() {
-    gulp.src(src+'/img/**/*')
-        .pipe(gulp.dest(dev+'/img'));
+gulp.task('imgsDev', function () {
+    gulp.src(src + '/img/**/*')
+        .pipe(gulp.dest(dev + '/img'));
 });
 
-gulp.task('imgsDist', function() {
-    gulp.src(src+'/img/**/*')
-        .pipe(gulp.dest(dist+'/img'));
+gulp.task('imgsDist', function () {
+    gulp.src(src + '/img/**/*')
+        .pipe(gulp.dest(dist + '/img'));
 });
 
 // fonts
-gulp.task('fontsDev', function() {
-    gulp.src(src+'/fonts/**/*')
-        .pipe(gulp.dest(dev+'/fonts'));
+gulp.task('fontsDev', function () {
+    gulp.src(src + '/fonts/**/*')
+        .pipe(gulp.dest(dev + '/fonts'));
 });
 
-gulp.task('fontsDist', function() {
-    gulp.src(src+'/fonts/**/*')
-        .pipe(gulp.dest(dist+'/fonts'));
+gulp.task('fontsDist', function () {
+    gulp.src(src + '/fonts/**/*')
+        .pipe(gulp.dest(dist + '/fonts'));
 });
 
 // css
-gulp.task('cssDev', function() {
-    gulp.src(src+'/css/**/*')
-        .pipe(gulp.dest(dev+'/css'));
+gulp.task('cssDev', function () {
+    gulp.src(src + '/css/**/*')
+        .pipe(gulp.dest(dev + '/css'));
 });
 
-gulp.task('cssDist', function() {
-    gulp.src(src+'/css/**/*')
-        .pipe(gulp.dest(dist+'/css'));
+gulp.task('cssDist', function () {
+    gulp.src(src + '/css/**/*')
+        .pipe(gulp.dest(dist + '/css'));
 });
 
 // js
-gulp.task('jsDev', function() {
-    gulp.src(src+'/js/app.js')
+gulp.task('jsDev', function () {
+    gulp.src(src + '/js/app.js')
         .pipe(browserify())
-        .pipe(gulp.dest(dev+'/js'));
+        .pipe(gulp.dest(dev + '/js'));
 });
 
-gulp.task('jsDist', function() {
-    gulp.src(src+'/js/app.js')
+gulp.task('jsDist', function () {
+    gulp.src(src + '/js/app.js')
         .pipe(browserify())
         .pipe(uglify())
-        .pipe(gulp.dest(dist+'/js'));
+        .pipe(gulp.dest(dist + '/js'));
 });
 
-gulp.task('dev', ['imgsDev','fontsDev','cssDev','jsDev']);
-gulp.task('dist', ['imgsDist','fontsDist','cssDist','jsDist']);
+gulp.task('dev', ['imgsDev', 'fontsDev', 'cssDev', 'jsDev']);
+gulp.task('dist', ['imgsDist', 'fontsDist', 'cssDist', 'jsDist']);
+gulp.task('dev-dist', ['dev', 'dist']);
+gulp.task('dev-dist', ['dev', 'dist']);
 
-gulp.task('watch', function() {
-    gulp.watch(src+'/img/**/*',['imgsDev', 'imgsDist']);
-    gulp.watch(src+'/fonts/**/*',['fontsDev', 'fontsDist']);
-    gulp.watch(src+'/css/**/*',['cssDev', 'cssDist']);
-    gulp.watch(src+'/js/**/*',['jsDev', 'jsDist']);
+gulp.task('watch', function () {
+    gulp.watch(src + '/img/**/*', ['imgsDev', 'imgsDist']);
+    gulp.watch(src + '/fonts/**/*', ['fontsDev', 'fontsDist']);
+    gulp.watch(src + '/css/**/*', ['cssDev', 'cssDist']);
+    gulp.watch(src + '/js/**/*', ['jsDev', 'jsDist']);
 });
