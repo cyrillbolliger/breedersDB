acf891025bf596a87698a315c8dc02867cca6c7c
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-09-16 17:49:36 +0200

    Fix: Make Mark Fields sortable again

--- webroot/src/js/app.js
+++ webroot/src/js/app.js
@@ -8,6 +8,11 @@ global.$ = global.jQuery = require('jquery');
  * load dependencies
  */
 require("jquery-ui");
+require("jquery-ui/ui/data");
+require("jquery-ui/ui/version");
+require("jquery-ui/ui/ie");
+require("jquery-ui/ui/scroll-parent");
+require("jquery-ui/ui/widget");
 require('jquery-ui/ui/widgets/mouse');
 require('jquery-ui/ui/widgets/datepicker');
 require('jquery-ui/ui/widgets/sortable');
