2af2eb07e080d0876961863a23bb7483c61d5954
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2017-09-22 19:02:59 +0200

    Queries: Nicely format mark values of type 'date'

--- src/Model/Entity/MarksView.php
+++ src/Model/Entity/MarksView.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\I18n\Date;
 
 /**
  * MarksView Entity
@@ -23,5 +24,21 @@ use Cake\ORM\Entity;
  */
 class MarksView extends Entity
 {
-
+    /**
+     * Return nicely formatted date if its a date. Else just return the value as it is.
+     * The conversion to the ymd format at saving is done in the beforeMarshalling method of the table.
+     *
+     * @return string
+     */
+    protected function _getValue()
+    {
+        $type = $this->_properties['field_type'];
+        
+        if ('DATE' === $type) {
+            $date = Date::parse($this->_properties['value']);
+            return $date->i18nFormat();
+        }
+        
+        return $this->_properties['value'];
+    }
 }
