6dc0a072fa91686d0cc571d08d0ccc99fd5bc53c
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2020-07-11 18:33:40 +0200

    upgrade to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -10,59 +10,58 @@ var dist = 'webroot/dist';
 
 // imgs
 gulp.task('imgsDev', function () {
-    gulp.src(src + '/img/**/*')
+    return gulp.src(src + '/img/**/*')
         .pipe(gulp.dest(dev + '/img'));
 });
 
 gulp.task('imgsDist', function () {
-    gulp.src(src + '/img/**/*')
+    return gulp.src(src + '/img/**/*')
         .pipe(gulp.dest(dist + '/img'));
 });
 
 // fonts
 gulp.task('fontsDev', function () {
-    gulp.src(src + '/fonts/**/*')
+    return gulp.src(src + '/fonts/**/*')
         .pipe(gulp.dest(dev + '/fonts'));
 });
 
 gulp.task('fontsDist', function () {
-    gulp.src(src + '/fonts/**/*')
+    return gulp.src(src + '/fonts/**/*')
         .pipe(gulp.dest(dist + '/fonts'));
 });
 
 // css
 gulp.task('cssDev', function () {
-    gulp.src(src + '/css/**/*')
+    return gulp.src(src + '/css/**/*')
         .pipe(gulp.dest(dev + '/css'));
 });
 
 gulp.task('cssDist', function () {
-    gulp.src(src + '/css/**/*')
+    return gulp.src(src + '/css/**/*')
         .pipe(gulp.dest(dist + '/css'));
 });
 
 // js
 gulp.task('jsDev', function () {
-    gulp.src(src + '/js/app.js')
+    return gulp.src(src + '/js/app.js')
         .pipe(browserify())
         .pipe(gulp.dest(dev + '/js'));
 });
 
 gulp.task('jsDist', function () {
-    gulp.src(src + '/js/app.js')
+    return gulp.src(src + '/js/app.js')
         .pipe(browserify())
         .pipe(uglify())
         .pipe(gulp.dest(dist + '/js'));
 });
 
-gulp.task('dev', ['imgsDev', 'fontsDev', 'cssDev', 'jsDev']);
-gulp.task('dist', ['imgsDist', 'fontsDist', 'cssDist', 'jsDist']);
-gulp.task('dev-dist', ['dev', 'dist']);
-gulp.task('dev-dist', ['dev', 'dist']);
+gulp.task('dev', gulp.parallel('imgsDev', 'fontsDev', 'cssDev', 'jsDev'));
+gulp.task('dist', gulp.parallel('imgsDist', 'fontsDist', 'cssDist', 'jsDist'));
+gulp.task('dev-dist', gulp.parallel('dev', 'dist'));
 
 gulp.task('watch', function () {
-    gulp.watch(src + '/img/**/*', ['imgsDev', 'imgsDist']);
-    gulp.watch(src + '/fonts/**/*', ['fontsDev', 'fontsDist']);
-    gulp.watch(src + '/css/**/*', ['cssDev', 'cssDist']);
-    gulp.watch(src + '/js/**/*', ['jsDev', 'jsDist']);
+    gulp.watch(src + '/img/**/*', gulp.parallel('imgsDev', 'imgsDist'));
+    gulp.watch(src + '/fonts/**/*', gulp.parallel('fontsDev', 'fontsDist'));
+    gulp.watch(src + '/css/**/*', gulp.parallel('cssDev', 'cssDist'));
+    gulp.watch(src + '/js/**/*', gulp.parallel('jsDev', 'jsDist'));
 });
