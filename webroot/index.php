a3cf1816ed261401ced74d0dff89720d3c440605
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2020-11-27 00:26:46 +0100

    unify platform checks

--- webroot/index.php
+++ webroot/index.php
@@ -14,6 +14,10 @@
  * @since         0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
+// Check platform requirements
+require dirname(__DIR__) . '/config/requirements.php';
+
 // for built-in server
 if (php_sapi_name() === 'cli-server') {
     $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
