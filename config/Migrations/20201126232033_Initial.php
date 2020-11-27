5da52370eb2a280fd092114da78c5824d0fba34e
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2020-11-27 21:08:46 +0100

    seed minimal data to get going

--- config/Migrations/20201126232033_Initial.php
+++ config/Migrations/20201126232033_Initial.php
@@ -1,5 +1,6 @@
 <?php
 
+use Cake\Auth\DefaultPasswordHasher;
 use Migrations\AbstractMigration;
 
 class Initial extends AbstractMigration {
@@ -1161,13 +1162,74 @@ class Initial extends AbstractMigration {
         /**
          * Handle views manually since Phinx can't handle them
          */
-        $this->execute( 'CREATE VIEW `batches_view` AS select `batches`.`id` AS `id`,concat(`crossings`.`code`,' . ',`batches`.`code`) AS `crossing_batch`,`batches`.`date_sowed` AS `date_sowed`,`batches`.`numb_seeds_sowed` AS `numb_seeds_sowed`,`batches`.`numb_sprouts_grown` AS `numb_sprouts_grown`,`batches`.`seed_tray` AS `seed_tray`,`batches`.`date_planted` AS `date_planted`,`batches`.`numb_sprouts_planted` AS `numb_sprouts_planted`,`batches`.`patch` AS `patch`,`batches`.`note` AS `note`,`batches`.`crossing_id` AS `crossing_id` from (`batches` join `crossings` on((`batches`.`crossing_id` = `crossings`.`id`))) where isnull(`batches`.`deleted`)' );
-        $this->execute( 'CREATE VIEW `crossings_view` AS select `crossings`.`id` AS `id`,`crossings`.`code` AS `code`,`mother_varieties`.`convar` AS `mother_variety`,`father_varieties`.`convar` AS `father_variety`,`crossings`.`target` AS `target` from ((`crossings` left join `varieties_view` `mother_varieties` on((`crossings`.`mother_variety_id` = `mother_varieties`.`id`))) left join `varieties_view` `father_varieties` on((`crossings`.`father_variety_id` = `father_varieties`.`id`))) where isnull(`crossings`.`deleted`)' );
-        $this->execute( 'CREATE VIEW `marks_view` AS select `mark_values`.`id` AS `id`,`marks`.`date` AS `date`,`marks`.`author` AS `author`,`marks`.`tree_id` AS `tree_id`,`marks`.`variety_id` AS `variety_id`,`marks`.`batch_id` AS `batch_id`,`mark_values`.`value` AS `value`,`mark_values`.`exceptional_mark` AS `exceptional_mark`,`mark_form_properties`.`name` AS `name`,`mark_form_properties`.`id` AS `property_id`,`mark_form_properties`.`field_type` AS `field_type`,`mark_form_property_types`.`name` AS `property_type` from (((`marks` join `mark_values` on((`marks`.`id` = `mark_values`.`mark_id`))) join `mark_form_properties` on((`mark_values`.`mark_form_property_id` = `mark_form_properties`.`id`))) join `mark_form_property_types` on((`mark_form_properties`.`mark_form_property_type_id` = `mark_form_property_types`.`id`)))' );
-        $this->execute( 'CREATE VIEW `mother_trees_view` AS select `mother_trees`.`id` AS `id`,`crossings`.`code` AS `crossing`,`mother_trees`.`code` AS `code`,`mother_trees`.`planed` AS `planed`,`mother_trees`.`date_pollen_harvested` AS `date_pollen_harvested`,`mother_trees`.`date_impregnated` AS `date_impregnated`,`mother_trees`.`date_fruit_harvested` AS `date_fruit_harvested`,`mother_trees`.`numb_portions` AS `numb_portions`,`mother_trees`.`numb_flowers` AS `numb_flowers`,`mother_trees`.`numb_fruits` AS `numb_fruits`,`mother_trees`.`numb_seeds` AS `numb_seeds`,`mother_trees`.`note` AS `note`,`trees_view`.`publicid` AS `publicid`,`trees_view`.`convar` AS `convar`,`trees_view`.`offset` AS `offset`,`trees_view`.`row` AS `row`,`trees_view`.`experiment_site` AS `experiment_site`,`mother_trees`.`tree_id` AS `tree_id`,`mother_trees`.`crossing_id` AS `crossing_id` from ((`mother_trees` join `trees_view` on((`mother_trees`.`tree_id` = `trees_view`.`id`))) join `crossings` on((`mother_trees`.`crossing_id` = `crossings`.`id`))) where isnull(`mother_trees`.`deleted`)' );
-        $this->execute( 'CREATE VIEW `scions_bundles_view` AS select `scions_bundles`.`id` AS `id`,`scions_bundles`.`code` AS `identification`,`varieties_view`.`convar` AS `convar`,`scions_bundles`.`numb_scions` AS `numb_scions`,`scions_bundles`.`date_scions_harvest` AS `date_scions_harvest`,`scions_bundles`.`descents_publicid_list` AS `descents_publicid_list`,`scions_bundles`.`note` AS `note`,`scions_bundles`.`external_use` AS `external_use`,`scions_bundles`.`variety_id` AS `variety_id` from (`scions_bundles` join `varieties_view` on((`scions_bundles`.`variety_id` = `varieties_view`.`id`))) where isnull(`scions_bundles`.`deleted`)' );
-        $this->execute( 'CREATE VIEW `trees_view` AS select `trees`.`id` AS `id`,`trees`.`publicid` AS `publicid`,`varieties_view`.`convar` AS `convar`,`trees`.`date_grafted` AS `date_grafted`,`trees`.`date_planted` AS `date_planted`,`trees`.`date_eliminated` AS `date_eliminated`,`trees`.`date_labeled` AS `date_labeled`,`trees`.`genuine_seedling` AS `genuine_seedling`,`trees`.`offset` AS `offset`,`rows`.`code` AS `row`,`trees`.`dont_eliminate` AS `dont_eliminate`,`trees`.`note` AS `note`,`trees`.`variety_id` AS `variety_id`,`graftings`.`name` AS `grafting`,`rootstocks`.`name` AS `rootstock`,`experiment_sites`.`name` AS `experiment_site` from (((((`trees` left join `rows` on((`trees`.`row_id` = `rows`.`id`))) left join `graftings` on((`trees`.`grafting_id` = `graftings`.`id`))) left join `rootstocks` on((`trees`.`rootstock_id` = `rootstocks`.`id`))) join `experiment_sites` on((`trees`.`experiment_site_id` = `experiment_sites`.`id`))) join `varieties_view` on((`trees`.`variety_id` = `varieties_view`.`id`))) where isnull(`trees`.`deleted`)' );
-        $this->execute( 'CREATE VIEW `varieties_view` AS select `varieties`.`id` AS `id`,concat(`batches_view`.`crossing_batch`,' . ',`varieties`.`code`) AS `convar`,`varieties`.`official_name` AS `official_name`,`varieties`.`acronym` AS `acronym`,`varieties`.`plant_breeder` AS `plant_breeder`,`varieties`.`registration` AS `registration`,`varieties`.`description` AS `description`,`varieties`.`batch_id` AS `batch_id` from (`varieties` join `batches_view` on((`varieties`.`batch_id` = `batches_view`.`id`))) where isnull(`varieties`.`deleted`)' );
+        $this->execute( "CREATE VIEW `batches_view` AS select `batches`.`id` AS `id`,concat(`crossings`.`code`,' . ',`batches`.`code`) AS `crossing_batch`,`batches`.`date_sowed` AS `date_sowed`,`batches`.`numb_seeds_sowed` AS `numb_seeds_sowed`,`batches`.`numb_sprouts_grown` AS `numb_sprouts_grown`,`batches`.`seed_tray` AS `seed_tray`,`batches`.`date_planted` AS `date_planted`,`batches`.`numb_sprouts_planted` AS `numb_sprouts_planted`,`batches`.`patch` AS `patch`,`batches`.`note` AS `note`,`batches`.`crossing_id` AS `crossing_id` from (`batches` join `crossings` on((`batches`.`crossing_id` = `crossings`.`id`))) where isnull(`batches`.`deleted`)" );
+        $this->execute( "CREATE VIEW `varieties_view` AS select `varieties`.`id` AS `id`,concat(`batches_view`.`crossing_batch`,' . ',`varieties`.`code`) AS `convar`,`varieties`.`official_name` AS `official_name`,`varieties`.`acronym` AS `acronym`,`varieties`.`plant_breeder` AS `plant_breeder`,`varieties`.`registration` AS `registration`,`varieties`.`description` AS `description`,`varieties`.`batch_id` AS `batch_id` from (`varieties` join `batches_view` on((`varieties`.`batch_id` = `batches_view`.`id`))) where isnull(`varieties`.`deleted`)" );
+        $this->execute( "CREATE VIEW `crossings_view` AS select `crossings`.`id` AS `id`,`crossings`.`code` AS `code`,`mother_varieties`.`convar` AS `mother_variety`,`father_varieties`.`convar` AS `father_variety`,`crossings`.`target` AS `target` from ((`crossings` left join `varieties_view` `mother_varieties` on((`crossings`.`mother_variety_id` = `mother_varieties`.`id`))) left join `varieties_view` `father_varieties` on((`crossings`.`father_variety_id` = `father_varieties`.`id`))) where isnull(`crossings`.`deleted`)" );
+        $this->execute( "CREATE VIEW `marks_view` AS select `mark_values`.`id` AS `id`,`marks`.`date` AS `date`,`marks`.`author` AS `author`,`marks`.`tree_id` AS `tree_id`,`marks`.`variety_id` AS `variety_id`,`marks`.`batch_id` AS `batch_id`,`mark_values`.`value` AS `value`,`mark_values`.`exceptional_mark` AS `exceptional_mark`,`mark_form_properties`.`name` AS `name`,`mark_form_properties`.`id` AS `property_id`,`mark_form_properties`.`field_type` AS `field_type`,`mark_form_property_types`.`name` AS `property_type` from (((`marks` join `mark_values` on((`marks`.`id` = `mark_values`.`mark_id`))) join `mark_form_properties` on((`mark_values`.`mark_form_property_id` = `mark_form_properties`.`id`))) join `mark_form_property_types` on((`mark_form_properties`.`mark_form_property_type_id` = `mark_form_property_types`.`id`)))" );
+        $this->execute( "CREATE VIEW `scions_bundles_view` AS select `scions_bundles`.`id` AS `id`,`scions_bundles`.`code` AS `identification`,`varieties_view`.`convar` AS `convar`,`scions_bundles`.`numb_scions` AS `numb_scions`,`scions_bundles`.`date_scions_harvest` AS `date_scions_harvest`,`scions_bundles`.`descents_publicid_list` AS `descents_publicid_list`,`scions_bundles`.`note` AS `note`,`scions_bundles`.`external_use` AS `external_use`,`scions_bundles`.`variety_id` AS `variety_id` from (`scions_bundles` join `varieties_view` on((`scions_bundles`.`variety_id` = `varieties_view`.`id`))) where isnull(`scions_bundles`.`deleted`)" );
+        $this->execute( "CREATE VIEW `trees_view` AS select `trees`.`id` AS `id`,`trees`.`publicid` AS `publicid`,`varieties_view`.`convar` AS `convar`,`trees`.`date_grafted` AS `date_grafted`,`trees`.`date_planted` AS `date_planted`,`trees`.`date_eliminated` AS `date_eliminated`,`trees`.`date_labeled` AS `date_labeled`,`trees`.`genuine_seedling` AS `genuine_seedling`,`trees`.`offset` AS `offset`,`rows`.`code` AS `row`,`trees`.`dont_eliminate` AS `dont_eliminate`,`trees`.`note` AS `note`,`trees`.`variety_id` AS `variety_id`,`graftings`.`name` AS `grafting`,`rootstocks`.`name` AS `rootstock`,`experiment_sites`.`name` AS `experiment_site` from (((((`trees` left join `rows` on((`trees`.`row_id` = `rows`.`id`))) left join `graftings` on((`trees`.`grafting_id` = `graftings`.`id`))) left join `rootstocks` on((`trees`.`rootstock_id` = `rootstocks`.`id`))) join `experiment_sites` on((`trees`.`experiment_site_id` = `experiment_sites`.`id`))) join `varieties_view` on((`trees`.`variety_id` = `varieties_view`.`id`))) where isnull(`trees`.`deleted`)" );
+        $this->execute( "CREATE VIEW `mother_trees_view` AS select `mother_trees`.`id` AS `id`,`crossings`.`code` AS `crossing`,`mother_trees`.`code` AS `code`,`mother_trees`.`planed` AS `planed`,`mother_trees`.`date_pollen_harvested` AS `date_pollen_harvested`,`mother_trees`.`date_impregnated` AS `date_impregnated`,`mother_trees`.`date_fruit_harvested` AS `date_fruit_harvested`,`mother_trees`.`numb_portions` AS `numb_portions`,`mother_trees`.`numb_flowers` AS `numb_flowers`,`mother_trees`.`numb_fruits` AS `numb_fruits`,`mother_trees`.`numb_seeds` AS `numb_seeds`,`mother_trees`.`note` AS `note`,`trees_view`.`publicid` AS `publicid`,`trees_view`.`convar` AS `convar`,`trees_view`.`offset` AS `offset`,`trees_view`.`row` AS `row`,`trees_view`.`experiment_site` AS `experiment_site`,`mother_trees`.`tree_id` AS `tree_id`,`mother_trees`.`crossing_id` AS `crossing_id` from ((`mother_trees` join `trees_view` on((`mother_trees`.`tree_id` = `trees_view`.`id`))) join `crossings` on((`mother_trees`.`crossing_id` = `crossings`.`id`))) where isnull(`mother_trees`.`deleted`)" );
+
+        /**
+         * Insert the necessary data to get going
+         */
+        // crossings
+        $data  = [
+            'id'                => '1',
+            'code'              => 'SORTE',
+            'mother_variety_id' => null,
+            'father_variety_id' => null,
+            'target'            => null,
+            'deleted'           => null,
+            'created'           => date( 'Y-m-d H:i:s' ),
+            'modified'          => date( 'Y-m-d H:i:s' ),
+        ];
+        $table = $this->table( 'crossings' );
+        $table->insert( $data )->save();
+
+        // batches
+        $data  = [
+            'id'                   => '1',
+            'code'                 => '000',
+            'date_sowed'           => null,
+            'numb_seeds_sowed'     => null,
+            'numb_sprouts_grown'   => null,
+            'seed_tray'            => null,
+            'date_planted'         => null,
+            'numb_sprouts_planted' => null,
+            'patch'                => null,
+            'note'                 => 'Reserviert für existierende Sorten',
+            'crossing_id'          => '1',
+            'deleted'              => null,
+            'created'              => date( 'Y-m-d H:i:s' ),
+            'modified'             => date( 'Y-m-d H:i:s' ),
+        ];
+        $table = $this->table( 'batches' );
+        $table->insert( $data )->save();
+
+        // users
+        $hasher = new DefaultPasswordHasher();
+        $data   = [
+            'id'        => '1',
+            'email'     => 'admin@breedersdb.com',
+            'password'  => $hasher->hash( 'ChangeAfterLogin' ),
+            'level'     => '0',
+            'time_zone' => 'Europe/Brussels',
+            'created'   => date( 'Y-m-d H:i:s' ),
+            'modified'  => date( 'Y-m-d H:i:s' ),
+        ];
+
+        $table = $this->table( 'users' );
+        $table->insert( $data )->save();
+
+        // experiment sites
+        $data = [
+            'id'   => '1',
+            'name' => 'Default Site',
+        ];
+
+        $table = $this->table( 'experiment_sites' );
+        $table->insert( $data )->save();
     }
 
     public function down() {
