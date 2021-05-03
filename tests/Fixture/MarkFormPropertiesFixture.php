1a549742a3585e84253cce86e0d01560d59b3164
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-05-03 22:51:35 +0200

    load dependent fixtures

--- tests/Fixture/MarkFormPropertiesFixture.php
+++ tests/Fixture/MarkFormPropertiesFixture.php
@@ -1,24 +1,28 @@
 <?php
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace App\Test\Fixture;
 
 use App\Generator\MarkFormPropertiesGenerator;
+use App\Test\Util\DependsOnFixtureTrait;
 use Cake\TestSuite\Fixture\TestFixture;
 
 /**
  * MarkFormPropertiesFixture
  */
-class MarkFormPropertiesFixture extends TestFixture
-{
+class MarkFormPropertiesFixture extends TestFixture {
+
+    use DependsOnFixtureTrait;
+
+    protected array $dependsOnFixture = [ 'MarkFormPropertyTypes' ];
+
     /**
      * Init method
      *
      * @return void
      */
-    public function init(): void
-    {
-        $generator = new MarkFormPropertiesGenerator();
+    public function init(): void {
+        $generator     = new MarkFormPropertiesGenerator();
         $this->records = $generator->generate();
         parent::init();
     }
