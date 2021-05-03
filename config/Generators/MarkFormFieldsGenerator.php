c876947f70d292d1887c09246ea7ead952c637dc
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-05-03 22:51:09 +0200

    make generator dynamic

--- config/Generators/MarkFormFieldsGenerator.php
+++ config/Generators/MarkFormFieldsGenerator.php
@@ -1,136 +1,35 @@
 <?php
+
 namespace App\Generator;
 
 /**
  * MarkFormFields generator.
  */
-class MarkFormFieldsGenerator
-{
+class MarkFormFieldsGenerator {
 
-    public function generate()
-    {
-        $data = [
-            [
-                'id' => '1',
-                'priority' => '0',
-                'mark_form_id' => '1',
-                'mark_form_property_id' => '7',
-                'created' => '2020-11-29 22:15:37',
-                'modified' => '2020-11-29 22:15:37',
-            ],
-            [
-                'id' => '2',
-                'priority' => '1',
-                'mark_form_id' => '1',
-                'mark_form_property_id' => '1',
-                'created' => '2020-11-29 22:15:37',
-                'modified' => '2020-11-29 22:15:37',
-            ],
-            [
-                'id' => '3',
-                'priority' => '3',
-                'mark_form_id' => '1',
-                'mark_form_property_id' => '21',
-                'created' => '2020-11-29 22:15:37',
-                'modified' => '2020-11-29 22:17:38',
-            ],
-            [
-                'id' => '4',
-                'priority' => '0',
-                'mark_form_id' => '2',
-                'mark_form_property_id' => '22',
-                'created' => '2020-11-29 22:16:21',
-                'modified' => '2020-11-29 22:16:21',
-            ],
-            [
-                'id' => '5',
-                'priority' => '1',
-                'mark_form_id' => '2',
-                'mark_form_property_id' => '11',
-                'created' => '2020-11-29 22:16:21',
-                'modified' => '2020-11-29 22:16:21',
-            ],
-            [
-                'id' => '6',
-                'priority' => '2',
-                'mark_form_id' => '2',
-                'mark_form_property_id' => '15',
-                'created' => '2020-11-29 22:16:21',
-                'modified' => '2020-11-29 22:16:21',
-            ],
-            [
-                'id' => '7',
-                'priority' => '3',
-                'mark_form_id' => '2',
-                'mark_form_property_id' => '21',
-                'created' => '2020-11-29 22:16:21',
-                'modified' => '2020-11-29 22:16:21',
-            ],
-            [
-                'id' => '8',
-                'priority' => '0',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '14',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '9',
-                'priority' => '1',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '17',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '10',
-                'priority' => '2',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '20',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '11',
-                'priority' => '3',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '16',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '12',
-                'priority' => '4',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '18',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '13',
-                'priority' => '5',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '2',
-                'created' => '2020-11-29 22:16:56',
-                'modified' => '2020-11-29 22:16:56',
-            ],
-            [
-                'id' => '14',
-                'priority' => '6',
-                'mark_form_id' => '3',
-                'mark_form_property_id' => '21',
-                'created' => '2020-11-29 22:17:15',
-                'modified' => '2020-11-29 22:17:15',
-            ],
-            [
-                'id' => '15',
-                'priority' => '2',
-                'mark_form_id' => '1',
-                'mark_form_property_id' => '3',
-                'created' => '2020-11-29 22:17:38',
-                'modified' => '2020-11-29 22:17:38',
-            ],
-        ];
+    public function generate( int $count = 50 ) {
+        $formsTable = \Cake\ORM\TableRegistry::getTableLocator()->get( 'MarkForms' );
+        $markForms  = $formsTable->find();
+
+        $propertiesTable = \Cake\ORM\TableRegistry::getTableLocator()->get( 'MarkFormProperties' );
+        $properties      = $propertiesTable->find();
+
+        $perForm = ceil( $markForms->count() / $count );
+
+        $data = [];
+        foreach ( $markForms as $form ) {
+            for ( $i = 0; $i < $perForm; $i ++ ) {
+                $data = [
+                    [
+                        'priority'              => $i,
+                        'mark_form_id'          => $form->id,
+                        'mark_form_property_id' => $properties->toArray()[$i]->id,
+                        'created'               => date('Y-m-d H:i:s'),
+                        'modified'              => date('Y-m-d H:i:s'),
+                    ]
+                ];
+            }
+        }
 
         return $data;
     }
