7d05b3ff507a42fe2d8bfa500670abf96d54d333
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-08-23 21:18:53 +0200

    add mobile mark to classic nav

--- config/routes.php
+++ config/routes.php
@@ -47,12 +47,8 @@ use Cake\Routing\Route\DashedRoute;
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
-    /**
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, src/Template/Pages/home.ctp)...
-     */
     $routes->connect('/', ['controller' => 'Trees', 'action' => 'index']);
+    $routes->connect('/spa/', ['controller' => 'Spa', 'action' => 'index']);
 
     /**
      * Connect catchall routes for all controllers.
