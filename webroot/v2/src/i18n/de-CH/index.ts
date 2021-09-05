e7e6481b7dc9229a90fb9288f3e1debc733c1224
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-09-05 23:56:18 +0200

    fix: use single quotes in translations

--- webroot/v2/src/i18n/de-CH/index.ts
+++ webroot/v2/src/i18n/de-CH/index.ts
@@ -1,70 +1,70 @@
 export default {
   marks: {
-    title: "Bewertungen",
+    title: 'Bewertungen',
     selectForm: {
-      title: "Formular auswählen",
-      tab: "Formular",
-      selected: "ausgewählt",
-      listMetaFiltered: "Gefilterte Liste. Zeige {showing} von {total} Elementen.",
-      listMetaUnfiltered: "{total} Elemente",
-      nothingFound: "Nichts gefunden"
+      title: 'Formular auswählen',
+      tab: 'Formular',
+      selected: 'ausgewählt',
+      listMetaFiltered: 'Gefilterte Liste. Zeige {showing} von {total} Elementen.',
+      listMetaUnfiltered: '{total} Elemente',
+      nothingFound: 'Nichts gefunden'
     },
     setMeta: {
-      title: "Meta-Daten festlegen",
+      title: 'Meta-Daten festlegen',
       tab: 'Meta',
-      author: "Autor",
-      authorHint: "Der Name der Person, die bewertet.",
-      date: "Datum",
-      dateHint: "Das Datum der Bewertung."
+      author: 'Autor',
+      authorHint: 'Der Name der Person, die bewertet.',
+      date: 'Datum',
+      dateHint: 'Das Datum der Bewertung.'
     },
     selectTree: {
-      title: "Baum auswählen",
-      tab: "Baum"
+      title: 'Baum auswählen',
+      tab: 'Baum'
     },
     markTree: {
-      title: "Baum bewerten",
-      tab: "Bewerten",
-      missingDataError: "Fehlende Daten.",
-      setMeta: "Meta-Daten hinzufügen",
-      selectTree: "Baum auswählen",
-      selectForm: "Formular auswählen",
-      saved: "Bewertungen gespeichert."
+      title: 'Baum bewerten',
+      tab: 'Bewerten',
+      missingDataError: 'Fehlende Daten.',
+      setMeta: 'Meta-Daten hinzufügen',
+      selectTree: 'Baum auswählen',
+      selectForm: 'Formular auswählen',
+      saved: 'Bewertungen gespeichert.'
     }
   },
   trees: {
     publicid: 'Publicid'
   },
   general: {
-    search: "Suchen",
-    loading: "Laden...",
-    retry: "Wiederholen",
-    failedToLoadData: "Fehler beim Laden der Daten.",
-    failedToSaveData: "Fehler beim Speichern der Daten.",
-    refreshList: "Liste aktualisieren",
-    next: "Nächste",
-    dismiss: "Verwerfen",
+    search: 'Suchen',
+    loading: 'Laden...',
+    retry: 'Wiederholen',
+    failedToLoadData: 'Fehler beim Laden der Daten.',
+    failedToSaveData: 'Fehler beim Speichern der Daten.',
+    refreshList: 'Liste aktualisieren',
+    next: 'Nächste',
+    dismiss: 'Verwerfen',
     navigation: 'Navigation',
     form: {
-      required: "Feld ist erforderlich",
-      max255chars: "Max. 255 Zeichen erlaubt",
-      save: "Speichern"
+      required: 'Feld ist erforderlich',
+      max255chars: 'Max. 255 Zeichen erlaubt',
+      save: 'Speichern'
     }
   },
   components: {
     util: {
       errorBanner: {
-        dismiss: "verwerfen"
+        dismiss: 'verwerfen'
       },
       treeCard: {
-        change: "Ändern",
-        selected: "Ausgewählter Baum"
+        change: 'Ändern',
+        selected: 'Ausgewählter Baum'
       }
     }
   },
   navigation: {
     markTrees: {
-      title: "Baum bewerten",
-      caption: "Scanne Bäume und bewerte sie."
+      title: 'Baum bewerten',
+      caption: 'Scanne Bäume und bewerte sie.'
     }
   }
 };
