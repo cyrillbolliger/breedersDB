8739d840bf175d5ad38e75741f84c5b8cec83358
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-12-19 23:04:56 +0100

    [BUILD] fix installation script

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ docker-compose pull
 docker-compose build app nodev2
 
 # install dependencies
-docker-compose run app composer install
+docker-compose run app composer install --no-interaction
 docker-compose run nodev1 npm install
-docker-compose run nodev2 sh -c 'yarn && quasar build'
+docker-compose run nodev2 yarn
 
 # initialize database
-docker-compose run app bin/cake bake migrations migrate --no-lock
-docker-compose run app bin/cake bake migrations migrate --no-lock --connection test
+docker-compose run app bin/cake migrations migrate --no-lock --no-interaction
+docker-compose run app bin/cake migrations migrate --no-lock --connection test --no-interaction
 
 # start up containers
 docker-compose up -d
