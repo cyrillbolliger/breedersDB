9a7737e860b5727e42d3cf3526850939a08b1240
Cyrill Bolliger <mail@cyrill.me>
2022-08-29 21:19:10 +0200

    [FIX] correct theme color

--- webroot/v2/quasar.conf.js
+++ webroot/v2/quasar.conf.js
@@ -160,7 +160,7 @@ module.exports = configure(function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#116D76',
         icons: [
           {
             src: 'icons/icon-128x128.png',
