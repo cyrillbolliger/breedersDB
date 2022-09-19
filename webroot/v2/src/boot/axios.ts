1c145af423b2e53539c50b20d4244b4aefa77247
Cyrill Bolliger <mail@cyrill.me>
2022-09-19 17:21:52 +0200

    [FIX] spellcheck

--- webroot/v2/src/boot/axios.ts
+++ webroot/v2/src/boot/axios.ts
@@ -18,7 +18,7 @@ declare const cake: {
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
+// good idea to move this instance creation inside the
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
