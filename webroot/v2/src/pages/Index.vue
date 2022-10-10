b973fd2f871b3722b72d66b70b68c2b33552e954
Cyrill Bolliger <mail@cyrill.me>
2022-10-10 21:37:11 +0200

    [FEAT] link to query page

--- webroot/v2/src/pages/Index.vue
+++ webroot/v2/src/pages/Index.vue
@@ -5,6 +5,11 @@
       color="primary"
       :label="t('marks.title')"
     />
+    <q-btn
+      to="/queries/new"
+      color="primary"
+      :label="t('queries.title')"
+    />
   </q-page>
 </template>
 
