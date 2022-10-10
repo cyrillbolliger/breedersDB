fd54802a3dc49f45a3a906db042bf43da1dd1c71
Cyrill Bolliger <mail@cyrill.me>
2022-10-10 21:34:03 +0200

    [FEAT] filter columns to add by typing

--- webroot/v2/src/components/Query/Result/ResultTableColumnSelector.vue
+++ webroot/v2/src/components/Query/Result/ResultTableColumnSelector.vue
@@ -3,15 +3,16 @@
     :disable="availableColumnsOption.length === 0"
     :label="t('queries.addColumn')"
     :model-value="null"
-    :options="availableColumnsOption"
+    :options="filteredOptions"
     use-input
+    @filter="filterFn"
     @update:model-value="option => showColumn(option.value)"
   />
 </template>
 <script lang="ts" setup>
 
 import {computed, PropType, ref, watch} from 'vue';
-import {QTableColumn} from 'quasar';
+import {QSelectProps, QTableColumn} from 'quasar';
 import {useI18n} from 'vue-i18n';
 import {useQueryStore} from 'stores/query';
 
@@ -65,10 +66,35 @@ const availableColumnsOption = computed<{ label: string, value: string }[]>(() =
     });
 });
 
+const filteredOptions = ref(availableColumnsOption.value);
+
 function showColumn(name: string) {
   if (-1 === visibleColumns.value.indexOf(name)) {
     visibleColumns.value = [...visibleColumns.value, name]
   }
+}
+
+function filterFn(value: string, update: Parameters<QSelectProps['onFilter']>[1]) {
+  update(
+    () => {
+      if (value === '') {
+        filteredOptions.value = availableColumnsOption.value
+      }
+      else {
+        const needle = value.toLocaleLowerCase();
+        filteredOptions.value = availableColumnsOption.value
+          .filter(option => option.label.toLocaleLowerCase().indexOf(needle) > -1)
+      }
+    },
+
+    // "ref" is the Vue reference to the QSelect
+    ref => {
+      if (value !== '' && ref.options && ref.options.length > 0) {
+        ref.setOptionIndex(-1)
+        ref.moveOptionSelection(1, true)
+      }
+    }
+  )
 }
 
 function resetVisibleColumns() {
