6456c5b37d7da69f5f1f5f4c86611ecca1b972d9
Cyrill Bolliger <mail@cyrill.me>
2022-12-10 21:01:51 +0100

    [FEAT] api: patch and delete

--- webroot/v2/src/composables/api.ts
+++ webroot/v2/src/composables/api.ts
@@ -2,7 +2,7 @@ import {i18n} from 'boot/i18n'
 import {Notify, QNotifyCreateOptions} from 'quasar'
 import {ref} from 'vue';
 import {api as axios} from 'boot/axios';
-import {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {AxiosError, AxiosRequestConfig} from 'axios';
 
 interface ApiResponse<T> {
   data: T
@@ -63,6 +63,56 @@ export default function useApi() {
       })
   }
 
+  function patch<T, R>(url: string, data: T, cb: () => void = () => null, config: AxiosRequestConfig = {}, handleErrors = true): Promise<void | R> {
+    working.value = true
+
+    let payload: T | { data: T }
+    if (data instanceof FormData) {
+      payload = data
+    } else {
+      payload = {data}
+    }
+
+    return axios.patch<T | { data: T }, ApiResponse<{data: R}>>(url, payload, config)
+      .then(resp => {
+        if (resp.data) {
+          cb()
+          return resp.data.data
+        }
+      })
+      .catch(error => {
+        working.value = false
+        cb()
+        if (handleErrors) {
+          return handleError<R>(error as AxiosError<R>, t('general.failedToSaveData'), () => patch<T, R>(url, data, cb, config))
+        } else {
+          return Promise.reject(error);
+        }
+      })
+  }
+
+  function remove<T>(url: string, cb: () => void = () => null, config: AxiosRequestConfig = {}, handleErrors = true): Promise<void | T> {
+    working.value = true
+
+    return axios.delete<void | ApiResponse<T>>(url, config)
+      .then(resp => {
+        cb()
+        working.value = false
+        if (resp.data) {
+          return resp.data.data
+        }
+      })
+      .catch(error => {
+        working.value = false
+        cb()
+        if (handleErrors) {
+          return handleError<T>(error as AxiosError<T>, t('general.failedToDeleteData'), () => remove<T>(url, cb, config))
+        } else {
+          return Promise.reject(error);
+        }
+      })
+  }
+
 
   function handleError<T>(error: AxiosError<T>, message: string, cb: () => Promise<void | T>) {
     console.log(error.message)
@@ -84,6 +134,8 @@ export default function useApi() {
     working,
     get,
     post,
+    patch,
+    delete: remove,
     handleError,
   }
 }
