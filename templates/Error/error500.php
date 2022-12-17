c9507bfb6ddcc3d3277c4ee7a2df439b8c70b3bc
Cyrill Bolliger <mail@cyrill.me>
2022-12-17 14:02:44 +0100

    [FIX] check xdebug mode before printing function stack

--- templates/Error/error500.php
+++ templates/Error/error500.php
@@ -7,10 +7,10 @@ $this->layout = 'error';
 
 if ( Configure::read( 'debug' ) ):
 	$this->layout = 'dev_error';
-	
+
 	$this->assign( 'title', $message );
 	$this->assign( 'templateName', 'error500.ctp' );
-	
+
 	$this->start( 'file' );
 	?>
 	<?php if ( ! empty( $error->queryString ) ) : ?>
@@ -29,11 +29,14 @@ if ( Configure::read( 'debug' ) ):
 <?php endif; ?>
 	<?php
 	echo $this->element( 'auto_table_warning' );
-	
-	if ( extension_loaded( 'xdebug' ) ):
+
+	if ( extension_loaded( 'xdebug' )
+        && ini_get('xdebug.mode')
+        && str_contains( ini_get('xdebug.mode'), 'develop' )
+    ):
 		xdebug_print_function_stack();
 	endif;
-	
+
 	$this->end();
 endif;
 ?>
