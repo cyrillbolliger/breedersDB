6a424e2b6f5c6ed0edb1d7ec542b177dcad638ea
Cyrill Bolliger <mail@cyrill.me>
2023-11-06 22:24:22 +0100

    [FEAT] expose batches

---
+++ src/Controller/REST1/BatchesController.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\REST1;
+
+use App\Controller\Component\JsonResponseComponent;
+use App\Controller\Component\PaginationQueryParamParserComponent;
+use App\Controller\REST1Controller;
+
+/**
+ * Batches Controller
+ *
+ * @property \App\Model\Table\BatchesTable $Batches
+ * @property JsonResponseComponent $JsonResponse
+ * @property PaginationQueryParamParserComponent $PaginationQueryParamParser
+ * @method \App\Model\Entity\Variety[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class BatchesController extends REST1Controller
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('PaginationQueryParamParser');
+    }
+
+    public function index()
+    {
+        if (!$this->request->is('get')) {
+            return $this->response
+                ->withStatus(405)
+                ->withAddedHeader('Allow', 'GET');
+        }
+
+        $term = $this->request->getQuery('term');
+        $limit = $this->PaginationQueryParamParser->getLimit();
+        $offset = $this->PaginationQueryParamParser->getOffset();
+        try {
+            $order = $this->PaginationQueryParamParser->getOrder();
+        } catch (\Exception $e) {
+            $this->JsonResponse->respondWithErrorJson([$e->getMessage()], 422);
+        }
+
+        $query = !empty($term)
+            ? $this->Batches->filterCrossingBatches($term)->contain(['Crossings']) ?? []
+            : $this->Batches->find()->contain(['Crossings']);
+
+        $dataQuery = $query->limit($limit)->offset($offset)->order($order);
+        $count = (clone $dataQuery)->count();
+
+        $this->set(['data' => [
+            'count' => $count,
+            'offset' => $offset ?? 0,
+            'sortBy' => !empty($order) ? array_keys($order)[0] : null,
+            'order' => !empty($order) ? array_values($order)[0] : null,
+            'limit' => $limit,
+            'results' => $dataQuery,
+        ]]);
+    }
+}
