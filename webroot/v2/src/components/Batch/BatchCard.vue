2df1e1ce0cf96e8f7ac78b0ceaa9ac0175646f2b
Cyrill Bolliger <mail@cyrill.me>
2023-11-19 21:45:45 +0100

    [FEAT] mark batches and varieties

---
+++ webroot/v2/src/components/Batch/BatchCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card bordered class="q-mb-md bg-grey-12" flat>
+    <q-card-section class="q-p-xs" horizontal>
+      <q-card-section class="col">
+        <div class="text-h6">{{ batch?.crossing_batch ?? t('components.batchCard.noBatch') }}</div>
+      </q-card-section>
+      <q-card-actions class="justify-around col-auto" vertical>
+        <q-btn
+          class="q-mt-xs"
+          color="primary"
+          flat
+          icon="search"
+          size="sm"
+          stack
+          @click="$emit('change', $event)"
+        >{{ t('components.util.search') }}
+        </q-btn>
+      </q-card-actions>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts" setup>
+import {useI18n} from 'vue-i18n';
+import {Batch} from 'src/models/batch';
+
+defineProps<{
+  batch?: Batch
+}>()
+
+defineEmits<{
+  change: (event: Event) => void
+}>()
+
+const {t} = useI18n() // eslint-disable-line @typescript-eslint/unbound-method
+</script>
---
+++ webroot/v2/src/pages/marks/MarkVariety.vue
@@ -0,0 +1,30 @@
+<template>
+  <MarkObj
+    :mark-obj-select-label="t('marks.markVariety.selectVariety')"
+    :title="t('marks.markVariety.title')"
+    :variety="variety || undefined"
+    form-select-url="/marks/variety/select-form"
+    mark-obj-select-url="/marks/variety/select-variety"
+    meta-set-url="/marks/variety/set-meta"
+  >
+    <template #obj-preview>
+      <VarietyCard
+        :variety="variety || undefined"
+        @change="$router.push('/marks/variety/select-variety')"
+      />
+    </template>
+  </MarkObj>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue'
+import {useI18n} from 'vue-i18n';
+import {useMarkStore} from 'stores/mark';
+import MarkObj from 'components/Mark/MarkObj.vue';
+import VarietyCard from 'components/Variety/VarietyCard.vue';
+
+const {t} = useI18n() // eslint-disable-line @typescript-eslint/unbound-method
+const store = useMarkStore()
+
+const variety = computed(() => store.variety)
+</script>
