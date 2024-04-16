31801e144b8e72330605a87e578efe929581afc1
Cyrill Bolliger <mail@cyrill.me>
2024-04-16 15:48:19 +0200

    [FEAT] hide scions bundles from nav

--- templates/element/top_nav.php
+++ templates/element/top_nav.php
@@ -3,8 +3,6 @@
     <li><?= $this->Html->link( __( 'Batches' ), [ 'controller' => 'Batches', 'action' => 'index' ] ) ?></li>
     <li><?= $this->Html->link( __( 'Varieties' ), [ 'controller' => 'Varieties', 'action' => 'index' ] ) ?></li>
     <li><?= $this->Html->link( __( 'Trees' ), [ 'controller' => 'Trees', 'action' => 'index' ] ) ?></li>
-    <li><?= $this->Html->link( __( 'Scions Bundles' ),
-			[ 'controller' => 'ScionsBundles', 'action' => 'index' ] ) ?></li>
     <li><?= $this->Html->link( __( 'Rows' ), [ 'controller' => 'Rows', 'action' => 'index' ] ) ?></li>
     <li><?= $this->Html->link( __( 'Marks' ), [ 'controller' => 'Marks', 'action' => 'index' ] ) ?></li>
     <li><?= $this->Html->link( __( 'Queries' ), [ 'controller' => 'Spa', 'action' => 'index', '#' => '/queries' ] ) ?></li>
